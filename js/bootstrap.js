17a8a68579a9d488357ec1bcad84fedd78bd511f
golfnut69 <brad@wikibetsolutions.com>
2017-05-21 12:42:08 +1000

    Add files via upload

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -1,1 +1,15 @@
+$(window).resize(function() {
+  $('.width').html($(document).width());
+});
 
+$(document).on("click", ".list1", function(){
+  $list = $(this).closest('.menuList');
+  $kids = $list.find('.list2');
+    if($list.hasClass('active')){
+        $list.removeClass('active');
+        $kids.hide();
+    }else{
+        $list.addClass('active');
+        $kids.fadeIn(1000);
+    }
+});
